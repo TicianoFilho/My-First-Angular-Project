17aa7a20e55ea9e63bb6234f3f95ef504263ba70
TicianoFilho <ticianofilho@gmail.com>
2023-01-21 19:05:36 -0300

    feat: add recipes component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from 'src/header/header.component';
+import { HeaderComponent } from 'src/app/header/header.component';
+import { RecipesComponent } from 'src/app/recipes/recipes.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    RecipesComponent,
   ],
   imports: [
     BrowserModule
